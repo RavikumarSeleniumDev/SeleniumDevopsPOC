6a613cc643d7701f28635bd38144f71e453099a0
RavikumarSeleniumDev <anireddyravi02@gmail.com>
2021-09-06 20:00:01 +0530

    Your commit message

--- src/test/java/testcase/Login.java
+++ src/test/java/testcase/Login.java
@@ -18,7 +18,7 @@ public class Login {
 	static WebDriver driver;
 	
 	@BeforeMethod
-	public void broserLaunch() {
+	public void browserLaunch() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Ravi_Old_Backup\\ravik\\eclipse\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -26,26 +26,30 @@ public class Login {
 		driver.get("http://automationpractice.com/index.php");
 		
 	}
-
-	/*
-	 * @Test public static void testcase1() { // TODO Auto-generated method stub
-	 * 
-	 * driver.findElement(By.className("login")).click();
-	 * driver.findElement(By.name("email")).sendKeys("testuser901@test.com");
-	 * driver.findElement(By.id("passwd")).sendKeys("test123");
-	 * driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span")).click();
-	 * 
-	 * }
-	 */
+@Test
+public static void  testcase1() {
+		// TODO Auto-generated method stub
+		
+		driver.findElement(By.className("login")).click();
+		driver.findElement(By.name("email")).sendKeys("testuser901@test.com");
+		driver.findElement(By.id("passwd")).sendKeys("test123");
+		driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.partialLinkText("sign out")).click();
+		
+		System.out.println("test case 1 passed where user can login and logout from application");
+		
+	    }
 @Test
 
 public static void testcase2() {
 	
 	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.className("login")).click();
 		driver.findElement(By.name("email")).sendKeys("testuser901@test.com");
 		driver.findElement(By.id("passwd")).sendKeys("test123");
-		driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span")).click();	
+		driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]/span")).click();
 		WebDriverWait wait = new WebDriverWait(Login.driver, 1);
 		wait.until(ExpectedConditions.visibilityOf(Login.driver.findElement(By.linkText("My addresses")))).click();
 		Login.driver.findElement(By.xpath("//*[@id=\"center_column\"]/div[2]/a")).click();
@@ -60,7 +64,7 @@ public static void testcase2() {
 		String TestABC = "Test" + new Random().nextInt(1000);
 		Login.driver.findElement(By.id("alias")).sendKeys(TestABC);
 		Login.driver.findElement(By.id("submitAddress")).click();
-		System.out.println("Test Case Passed");
+		System.out.println("Test Case2 Passed - where user can create a new address");
 	}
 }
 @AfterMethod
