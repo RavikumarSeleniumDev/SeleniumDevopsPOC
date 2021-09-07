680f24a00a58d404e29f0239af3ef1f0affffc4f
RavikumarSeleniumDev <anireddyravi02@gmail.com>
2021-09-07 17:11:49 +0530

    testase update2

--- src/test/java/testcase/Login.java
+++ src/test/java/testcase/Login.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.Reporter;
+
 
 
 public class Login {
